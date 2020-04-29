f65fb03090d2707dc793d86f5af05b2c08c8d6f5
Zach <zach.a.choate@gmail.com>
2020-04-29 14:11:10 -0400

    Name update

--- certify/Install-LECertify-LDAPS.ps1
+++ certify/Install-LECertify-LDAPS.ps1
@@ -8,7 +8,7 @@
     Last updated:   04/29/2020
     Creation date:  04/29/2020
     Author:         Zachary Choate
-    URL:            https://raw.githubusercontent.com/zchoate/letsencrypt-scripts/master/certify/Install-LE-LDAPS-Certify.ps1
+    URL:            https://raw.githubusercontent.com/zchoate/letsencrypt-scripts/master/certify/Install-LECertify-LDAPS.ps1
 #>
 
 param($result)
