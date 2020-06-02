adf2474bbfe688c5fba04f52ff3774b6064845d1
Zach <zach.a.choate@gmail.com>
2020-06-02 00:26:23 -0400

    Update Install-LECertify-MsSql.ps1
    Added in logic to get account used to start service and grant read access to private key to that account.

--- certify/Install-LECertify-MsSql.ps1
+++ certify/Install-LECertify-MsSql.ps1
@@ -4,13 +4,12 @@
     Install certificate generated from CertifyTheWeb to MSSQL instances and restart associated instances.
     Script is service impacting as SQL services are restarted. Schedule renewals accordingly.
 .NOTES
-    Version:        0.1
+    Version:        0.2
     Last updated:   06/01/2020
     Creation date:  06/01/2020
     Author:         Zachary Choate
-    URL:            https://raw.githubusercontent.com/zchoate/letsencrypt-scripts/master/certify/Install-LECertifyMsSql.ps1
+    URL:            https://raw.githubusercontent.com/zchoate/letsencrypt-scripts/master/certify/Install-LECertify-MsSql.ps1
 #>
-
 
 param($result)
 
@@ -27,10 +26,27 @@ foreach($instance in $instances) {
 
     }
 
+# Get certificate private key path to use to set ACLs later on.
+$certificate = Get-ChildItem "Cert:\LocalMachine\My" | Where-Object {$_.thumbprint -eq $thumbprint}
+$privateKey = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($certificate)
+$keyFile = $privateKey.Key.UniqueName
+$keyPath = "$env:ALLUSERSPROFILE\Microsoft\Crypto\RSA\MachineKeys\$keyFile"
+
+# Get MsSql services
 $services = Get-Service | Where-Object {$_.Name -match ($instances -Join "|") -and $_.DisplayName -like "*SQL Server*" -and $_.DisplayName -notlike "*SQL Server Agent*" -and $_.DisplayName -notlike "*SQL Server Analysis Services*"}
 
 foreach($service in $services) {
 
+    # Get service account used to start service
+    $acct = Get-WmiObject win32_service | Where-Object {$_.Name -like "$($service.Name)"} | Select-Object StartName
+
+    # Set private key ACLs for service accounts.
+    $permissions = Get-Acl -Path $keyPath
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($acct.StartName, 'Read', 'None', 'None', 'Allow')
+    $permissions.AddAccessRule($accessRule)
+    Set-Acl -Path $keyPath -AclObject $permissions
+
+    # restart services
     Restart-Service $service.Name -Force
 
     }
