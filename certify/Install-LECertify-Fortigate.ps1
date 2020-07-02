0750eeafe9f70ebf76aece6c1dde0aa377dfbcc8
Zach Choate <zach.a.choate@gmail.com>
2020-07-02 14:20:13 -0400

    Updates to add intermediate ca
    Add intermediate CA issuance.
    Added handling of expected errors for existing intermediate CA installed and dropped admin connection when rebinding.

--- certify/Install-LECertify-Fortigate.ps1
+++ certify/Install-LECertify-Fortigate.ps1
@@ -1,11 +1,15 @@
-<#   
-    .DESCRIPTION
-        Add certificate from CertifyTheWeb to Fortigate.
-        Apply certificate for use on VPN.
-        Apply certificate as admin interface certificate.
-        Remove old certificate.
-    .NOTES
-        Refer to https://github.com/KSMC-TS/docs/blob/main/fortinet/AutomatedCertificateRenewal/readme.md for setup instructions.
+<#
+
+.DESCRIPTION
+    Add certificate from CertifyTheWeb to Fortigate.
+    Apply certificate for use on VPN.
+    Apply certificate as admin interface certificate.
+    Remove old certificate.
+.NOTES
+    Refer to https://github.com/KSMC-TS/docs/blob/main/fortinet/AutomatedCertificateRenewal/readme.md for setup instructions.
+
+    Updated:    07/02/2020
+    Author:     Zach Choate
         
 #>
 
@@ -18,8 +22,13 @@ $encryptedToken = "PathToTextFileContainingToken"
 $apiToken = Get-Content $encryptedToken | ConvertTo-SecureString -Key (1..16)
 $sourcePFX = $result.ManagedItem.CertificatePath
 
-$pfx = Get-Content $sourcePFX -Encoding Byte
-$pfx = [System.Convert]::ToBase64String($pfx)
+# Get PFX and strip private key and export for use to import into Fortigate.
+$pfxObject = New-Object -TypeName 'System.Security.Cryptography.X509Certificates.X509Certificate2Collection'
+$pfxObject.Import($sourcePfx,$null,[System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet)
+$caCert = [System.Convert]::ToBase64String($($pfxObject[0].Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert)))
+
+# Get PFX as base64 string
+$pfx = [System.Convert]::ToBase64String($(Get-Content $sourcePFX -Encoding Byte))
 
 # Ignore invalid certificate warning
 add-type @"
@@ -59,6 +68,16 @@ $newCertBody = @{
     }
 $newCertJson = $newCertBody | ConvertTo-Json
 
+# Upload CA cert
+$caCertPath = "/monitor/vpn-certificate/ca/import"
+$caCertUrl = "$baseUrl$newCertPath"
+$caCertBody = @{
+    import_method="file"
+    scope="global"
+    file_content=$caCert
+    }
+$caCertJson = $caCertBody | ConvertTo-Json
+
 # Change active cert on VPN
 $vpnSettingsPath = "/cmdb/vpn.ssl/settings"
 $vpnSettingsUrl = "$baseUrl$vpnSettingsPath"
@@ -81,13 +100,26 @@ $currentCert = $currentCert.results.servercert
 
 # Execute post for uploading new cert
 Invoke-RestMethod -Uri $newCertUrl -Headers $requestHeader -Body $newCertJson -Method Post -ContentType 'application/x-www-form-urlencoded'
+Start-Sleep -Seconds 1
+
+# Execute post for uploading new CA cert
+Try {
+    Invoke-RestMethod -Uri $caCertUrl -Headers $requestHeader -Body $caCertJson -Method Post -ContentType 'application/x-www-form-urlencoded' -ErrorAction SilentlyContinue
+} catch {
+    Write-Output "Intermediate CA cert failed on upload. This is likely a result of it already existing on the Fortigate."
+}
+Start-Sleep -Seconds 1
 
 # Execute put for updating active vpn cert
 Invoke-RestMethod -Uri $vpnSettingsUrl -Headers $requestHeader -Body $vpnSettingsJson -Method Put -ContentType 'application/x-www-form-urlencoded'
+Start-Sleep -Seconds 1
 
 # Execute put for updating active admin cert
-Invoke-RestMethod -Uri $adminSettingsUrl -Headers $requestHeader -Body $adminSettingsJson -Method Put -ContentType 'application/x-www-form-urlencoded' -ErrorAction SilentlyContinue
-
+Try {
+    Invoke-RestMethod -Uri $adminSettingsUrl -Headers $requestHeader -Body $adminSettingsJson -Method Put -ContentType 'application/x-www-form-urlencoded' -ErrorAction SilentlyContinue
+} catch {
+    Write-Output "The connection to $adminSettingsUrl dropped. This is to be expected as it is rebinding the certificate."
+}
 Start-Sleep -Seconds 5
 
 # Remove old certificate
